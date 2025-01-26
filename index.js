691136037c5df683f2b3965b5805ba50d18ca705
C0d1ng-Wiz <acarbalkan503@gmail.com>
2025-01-26 15:22:04 -0500

    Update index.js
    Modified cardReduction so it has a comma for better reading. Created backersDisplay variable with template literal that include cardReduction. Put backersDisplay inside contributionsCard through contributionsCard.innerHTML = backersDisplay.

--- index.js
+++ index.js
@@ -75,7 +75,13 @@ let cardReduction = GAMES_JSON.reduce((acc, current) => {
 }, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+cardReduction=cardReduction.toLocaleString('en-US');
 
+const backersDisplay = `
+      <p>${cardReduction}</p>
+      `;
+
+contributionsCard.innerHTML = backersDisplay;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
